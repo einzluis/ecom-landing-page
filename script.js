3ed49c6a1e8fed2f48f033a84f0fa02ccba6e915
einzluis <einzluisboticario@gmail.com>
2021-07-31 22:34:58 +0800

    Moved scripts to scripts.js

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+// JavaScript Menu Toggle
+var MenuItems = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+    if (MenuItems.style.maxHeight === "0px") {
+        MenuItems.style.maxHeight = "200px"
+    } else {
+        MenuItems.style.maxHeight = "0px";
+    }
+}
+
+// JavaScript Product Gallery
+let ProductImg = document.getElementById("ProductImg");
+let SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function () {
+    ProductImg.src = smallImage[0].src;
+}
+SmallImg[1].onclick = function () {
+    ProductImg.src = smallImage[1].src;
+}
+SmallImg[2].onclick = function () {
+    ProductImg.src = smallImage[2].src;
+}
+SmallImg[3].onclick = function () {
+    ProductImg.src = smallImage[3].src;
+}
